b378e87165c86ca9060b82fdeccd1fac31c81c3d
JaveriyaQuadri <javeriyaquadri16@gmail.com>
2025-11-22 11:20:52 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!123456");
+        System.out.println("Hello, World!12345678");
     }
 }
